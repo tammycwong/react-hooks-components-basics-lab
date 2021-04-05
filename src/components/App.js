9d00033f5c22323022f49475fe49536892649d5c
Tammy <tammycwong@gmail.com>
2021-04-05 10:24:37 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -15,13 +15,20 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div id ="about"> 
+      <h2>About Me:</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      {<Home />}
+      {<About />}
     </div>
   );
 }
